d095810a6e750c03a2e7440a20b147653df1184c
private-yusuke <townstock@ksf.biglobe.ne.jp>
2019-12-15 12:29:30 +0900

    06 in D, create README.md

---
+++ chapter01/knock06.d
@@ -0,0 +1,48 @@
+import std;
+
+// 参考にしました
+// https://furutsuki.hatenablog.com/entry/2018/02/01/233840
+auto ngram(Range)(Range r, ulong n = 2) if (isInputRange!(Unqual!Range)) {
+    assert(n >= 1);
+
+    // InputRange として考える。
+    // empty, popFront, front を持っていればよい
+
+    static struct Result {
+        private alias R = Unqual!Range;
+        private size_t n;
+        private size_t k;
+
+        public R source;
+        this(R source, size_t n) {
+            this.source = source;
+            this.n = n;
+        }
+
+        @property {
+            bool empty() {
+                return this.k + this.n - 1 >= this.source.length;
+            }
+            auto ref front() {
+                return this.source[k..k+n];
+            }
+        }
+
+        void popFront() {
+            this.k++;
+        }
+    }
+    return Result(r, n);
+}
+
+void main() {
+    auto X = ngram("paraparaparadise").array.sort.uniq.array;
+    auto Y = ngram("paragraph").array.sort.uniq.array;
+
+    writefln("X or Y: %s", multiwayUnion([X, Y]));
+    writefln("X and Y: %s", setIntersection(X, Y));
+    writefln("X - Y: %s", X.setDifference(Y));
+    writefln("Y - X: %s", Y.setDifference(X));
+    writefln("'se' in X?: %s", X.canFind("se"));
+    writefln("'se' in Y?: %s", Y.canFind("se"));
+}
