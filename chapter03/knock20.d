387387128e56b4bf5a8bb55bc495415dd8837f51
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2019-12-15 16:52:04 +0900

    20, 21, 22, 23, 24, 25

---
+++ chapter03/knock20.d
@@ -0,0 +1,21 @@
+import std;
+
+JSONValue[] getJson() {
+    auto uc = new UnCompress;
+
+    string content;
+    foreach(chunk; File("jawiki-country.json.gz").byChunk(1024)) {
+        auto unc = uc.uncompress(chunk);
+        content ~= cast(string) unc;
+    }
+    content ~= cast(string) uc.flush();
+
+    return content.chomp.split('\n').map!(i => parseJSON(i)).array;
+}
+
+void main() {
+    auto json = getJson();
+    foreach(v; json) {
+        if(v["title"].str == "イギリス") writeln(v["text"].str);
+    }
+}
---
+++ chapter03/knock22.d
@@ -0,0 +1,24 @@
+import std;
+
+JSONValue[] getJson() {
+    auto uc = new UnCompress;
+
+    string content;
+    foreach(chunk; File("jawiki-country.json.gz").byChunk(1024)) {
+        auto unc = uc.uncompress(chunk);
+        content ~= cast(string) unc;
+    }
+    content ~= cast(string) uc.flush();
+
+    return content.chomp.split('\n').map!(i => parseJSON(i)).array;
+}
+
+string UK() {
+    return getJson().filter!(i => i["title"].str == "イギリス").array[0]["text"].str;
+}
+
+void main() {
+    string uk = UK();
+    auto reg = ctRegex!(r".*\[\[Category:(.*?)(?:\|.*)?\]\]");
+    uk.matchAll(reg).map!(v => v[1]).each!writeln;
+}
