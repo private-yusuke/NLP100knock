67173cdf36793d8df293fc11a383f4cd8d6e37d0
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2019-12-16 22:37:36 +0900

    26

---
+++ chapter03/knock26.d
@@ -0,0 +1,35 @@
+import std;
+
+JSONValue[] getJson() {
+    auto uc = new UnCompress;
+
+    string content;
+    foreach(chunk; File("jawiki-country.json.gz").byChunk(1024)) {
+        auto unc = uc.uncompress(chunk);
+        content ~= cast(string) unc;
+    }
+    content ~= cast(string) uc.flush();
+
+    return content.chomp.split('\n').map!(i => parseJSON(i)).array;
+}
+
+string UK() {
+    return getJson().filter!(i => i["title"].str == "イギリス").array[0]["text"].str;
+}
+
+void main() {
+    string uk = UK();
+    auto reg1 = ctRegex!(r"^\{\{基礎情報.*?$(.*?)^\}\}$", "gms");
+    auto basicinfos = uk.matchAll(reg1).array[0][1];
+    auto removeSign = ctRegex!(r"('{2,5})(.+?)(\1)", "gms");
+    string removeSignFun(Captures!string c) {
+        return c[2];
+    }
+    basicinfos = replaceAll!(removeSignFun)(basicinfos, removeSign);
+    auto reg2 = ctRegex!(r"\|(.+?) = (.+?)(?:(?=\n\|)|(?=\|$))", "gms");
+    string[string] m;
+    foreach(v; basicinfos.matchAll(reg2)) {
+        m[v[1]] = v[2];
+    }
+    m.each!((i, v) => writefln("%s: %s", i, v));
+}
